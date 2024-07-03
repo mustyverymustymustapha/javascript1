cb2ae56157d825e6d68dd6dfdfd5a2e84afe5028
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-07-03 07:55:54 +0000

    Initial Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+/* -ternary operators- */
+// ternary operators let you write a conditional/if statement using "?" and ":". ? is used first and if the condition is true, the code after ? is executed, if its false, the code after : is executed. for example:
+let HackClubIsCool = true;
+HackClubIsCool ? console.log('Hack Club is cool!') : console.log('Hack Club is not cool );')
+
+
+/* -else if statements- */
+// after an if block, you can add an else if block that checks one new condition each time. you can add an else block after the else if blocks to execute a different piece of code if none of the conditions are true. for example:
+let BestHackClubReviewer = 'Devlos'
+// (these are some people who have reviewed my projects)
+if (BestHackClubReviewer === 'fayd') {
+  console.log('Fayd is the best reviewer!');
+} else if(BestHackClubReviewer === 'Dillon') {
+  console.log('Dillon is the best reviewer!');
+} else if(BestHackClubReviewer === 'Phaedra') {
+  console.log('Phaedra is the best reviewer!');
+} else if(BestHackClubReviewer === 'Devlos') {
+  console.log('Devlos is the best reviewer!');
+} else {
+  console.log('No one is a good reviewer );');
+}
+
+
+
+// this is all i could do in an hour, im gonna be honest im a slow learner, or relearner for that matter. but i hope you enjoyed reading this
